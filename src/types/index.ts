52f29e3ec963f43410ea7527bd5be593bcc68e52
GabrieleRossiITS <gabriele.rossi.25@stud.itsaltoadriatico.it>
2026-03-26 20:40:05 +0100

    feat(types): add Artwork and Exhibition interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Artwork {
+    id: number;
+    title: string;
+    year: number;
+    image: string;
+}
+
+export interface Exhibition {
+    id: number;
+    title: string;
+    location: string;
+    coordinates: {
+        lat: number;
+        lng: number;
+    };
+    maps_url: string;
+    status: "ongoing" | "past" | "upcoming";
+    start_date: string;
+    end_date: string;
+    description: string;
+    artworks_included: number[];
+}
